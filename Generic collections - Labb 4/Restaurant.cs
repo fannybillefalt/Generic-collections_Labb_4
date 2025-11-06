5ca92a0d1ea817d205dedd0af0b930ddda4c11a4
fannybillefalt <fannybillefalt@gmail.com>
2025-11-06 11:00:36 +0100

    Created Restaurant class and added a list and a queue

---
+++ Generic collections - Labb 4/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_collections___Labb_4
+{
+    internal class Restaurant
+    {
+        List<MenuItem> Menu = new List<MenuItem>();
+        Queue<Order> OrderQueue = new Queue<Order>();
+    }
+}
