18b3070b1523ed27a870d50aa1e42730d85cd07a
fannybillefalt <fannybillefalt@gmail.com>
2025-11-06 10:51:12 +0100

    Add project files.

---
+++ Generic collections - Labb 4/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_collections___Labb_4
+{
+    internal class MenuItem
+    {
+    }
+}
---
+++ Generic collections - Labb 4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic_collections___Labb_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
