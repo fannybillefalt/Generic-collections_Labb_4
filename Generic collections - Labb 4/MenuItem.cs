0717cb98a8c00b7be63077cba7bb1f5621c4f338
fannybillefalt <fannybillefalt@gmail.com>
2025-11-06 10:54:29 +0100

    Created MenuItem class

--- Generic collections - Labb 4/MenuItem.cs
+++ Generic collections - Labb 4/MenuItem.cs
@@ -8,5 +8,21 @@ namespace Generic_collections___Labb_4
 {
     internal class MenuItem
     {
+        // I denna klass använder vi auto-implemented properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price:C}";
+        }
     }
 }
